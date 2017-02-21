b9ed0cb57550ad95036170be0269ee1e9b2a86de
Chengming Lu <cluster626@gmail.com>
2017-02-21 16:09:57 -0500

    Added start button Improved velocity spread for rocks randomly spawn rocks upon tapping

--- Unprotected/GameScene.swift
+++ Unprotected/GameScene.swift
@@ -11,12 +11,20 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     var touchInProgress = false
+    var gameInProgress = false
     var earth = SKSpriteNode()
     var earthShield = SKSpriteNode()
-    var rock = SKSpriteNode()
+    var startButton = SKSpriteNode()
+    //var rock = SKSpriteNode()
+    var rockTexture = SKTexture()
+    var timerNode = SKNode()
     var rockVelocity = CGFloat()
+    var rockSpawnInterval = TimeInterval(5.0)
+    var halfFrameDiagnal = CGFloat()
+    var dynamicDifficulty = CGFloat()
+    var timer = CGFloat(0)
+    var diagnalAngle = CGFloat()
     let objectRelativeScale = CGFloat(0.0002)
-    //let rockScale = CGFloat(0.10)
     let rockVelocityScale = CGFloat(0.3)
     let initialShieldOffset = CGFloat(-1.82)
     let rotationAngle = CGFloat(M_PI / 32)
@@ -27,20 +35,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         case Shield = 2
         case Earth = 4
     }
+    
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        halfFrameDiagnal = sqrt(pow(self.frame.size.width / 2, 2) + pow(self.frame.size.height / 2, 2))
+        diagnalAngle = tanh(self.frame.size.width / self.frame.size.height)
+        
+        //timerNode.zPosition = -1
+        //testing
+        //let testEarth = SKShapeNode(circleOfRadius: 10)
+        //testEarth.fillColor = SKColor.green
+        //testEarth.strokeColor = SKColor.clear
+        //testEarth.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        //finished testing
         
         let earthTexture = SKTexture(imageNamed: "EarthFG.png")
         let earthShieldTexture = SKTexture(imageNamed: "EarthShieldSingle.png")
-        let rockTexture = SKTexture(imageNamed: "Rock.png")
+        let startButtonTexture = SKTexture(imageNamed: "StartButton.png")
+        rockTexture = SKTexture(imageNamed: "Rock.png")
         earth = SKSpriteNode(texture: earthTexture)
         earthShield = SKSpriteNode(texture: earthShieldTexture)
-        rock = SKSpriteNode(texture: rockTexture)
+        startButton = SKSpriteNode(texture: startButtonTexture)
+        
         earth.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         earthShield.position = earth.position
+        startButton.position = earth.position
         earth.setScale(self.frame.size.width * objectRelativeScale)
         earthShield.setScale(earth.xScale)
-        rock.setScale(earth.xScale)
+        startButton.setScale(earth.xScale)
+        
         earthShield.zRotation = initialShieldOffset
         earth.physicsBody = SKPhysicsBody(circleOfRadius: earth.size.width / 2, center: earth.position)
         earth.physicsBody!.isDynamic = false
@@ -52,29 +75,69 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         earthShield.physicsBody!.categoryBitMask = CollisionType.Shield.rawValue
         earthShield.physicsBody!.contactTestBitMask = CollisionType.Rock.rawValue
         earthShield.physicsBody!.collisionBitMask = CollisionType.Rock.rawValue
+        
+        //we can actually add shield here , just hide it behind the earth, better for animation
+        
+        self.addChild(earth)
+        self.addChild(startButton)
+        
+        //self.frame:width = 750, height = 1334
+        
+        
+        //testing
+        //self.addChild(testEarth)
+        //finished testing
+    }
+    
+    func spawnRockWithVelocity(Pos: CGPoint) {
+        var rock = SKSpriteNode()
+        rock = SKSpriteNode(texture: rockTexture)
+        rock.setScale(earth.xScale)
         rock.physicsBody = SKPhysicsBody(circleOfRadius: rock.size.width / 2)
         rock.physicsBody!.affectedByGravity = false
         rock.physicsBody!.categoryBitMask = CollisionType.Rock.rawValue
         rock.physicsBody!.contactTestBitMask = CollisionType.Earth.rawValue | CollisionType.Shield.rawValue
         rock.physicsBody!.collisionBitMask = CollisionType.Earth.rawValue | CollisionType.Shield.rawValue
-        self.addChild(earth)
-        self.addChild(earthShield)
-        //self.frame:width = 750, height = 1334
-        spawnObjectWithVelocity(object: rock, initialPos: CGPoint(x: self.frame.midX - self.frame.width / 2, y: self.frame.midY - self.frame.height / 2))
-    }
-    
-    func spawnObjectWithVelocity(object: SKSpriteNode, initialPos: CGPoint) {
-        object.position = initialPos
-        self.addChild(object)
-        let velocity = CGVector(dx: (self.frame.midX - initialPos.x) * rockVelocityScale, dy: (self.frame.midY - initialPos.y) * rockVelocityScale)
-        object.physicsBody!.velocity = velocity
+        
+        
+        //object = originalObject.copy()
+        rock.position = Pos
+        let velocity = CGVector(dx: (self.frame.midX - Pos.x) * rockVelocityScale, dy: (self.frame.midY - Pos.y) * rockVelocityScale)
+        rock.physicsBody!.velocity = velocity
+        self.addChild(rock)
+        print("spawned a new rock at \(Pos)) with velocity of \(velocity)")
+        //object.position.x += 30
+        //self.addChild(object)
         //print(object.physicsBody!.velocity)
     }
     
-    func spawnObjectRandomly() {
+    func spawnRockRandomly() {
+        let randomDegree = CGFloat(2 * M_PI * drand48())
+        let randomizedSpawnPoint = degreeToRandomizedPointOnRectangle(degreeInRadians: randomDegree, radius: halfFrameDiagnal)
+        spawnRockWithVelocity(Pos: randomizedSpawnPoint)
     //
     }
     
+    func degreeToRandomizedPointOnRectangle(degreeInRadians: CGFloat, radius: CGFloat) -> CGPoint {
+        var xPos = CGFloat()
+        var yPos = CGFloat()
+        if degreeInRadians > CGFloat(2 * M_PI) - diagnalAngle || degreeInRadians <= diagnalAngle {
+            xPos = self.frame.size.height / 2
+            yPos = xPos * tan(degreeInRadians)
+        } else if degreeInRadians > diagnalAngle && degreeInRadians <= CGFloat(M_PI) - diagnalAngle {
+            yPos = self.frame.size.width / 2
+            xPos = yPos / tan(degreeInRadians)
+        } else if degreeInRadians > CGFloat(M_PI) - diagnalAngle && degreeInRadians <= CGFloat(M_PI) + diagnalAngle {
+            xPos = -self.frame.size.height / 2
+            yPos = xPos * tan(degreeInRadians)
+        } else {
+            yPos = -self.frame.size.width / 2
+            xPos = yPos / tan(degreeInRadians)
+        }
+        return CGPoint(x: xPos, y: yPos)
+    }
+    
+
     
     func toggleRotationClockwise(object: SKSpriteNode) {
         object.zRotation -= rotationAngle
@@ -87,8 +150,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if contact.bodyA.categoryBitMask == CollisionType.Earth.rawValue {
             print("absorbed by earth")
         }
-        rock.removeFromParent()
-        spawnObjectWithVelocity(object: rock, initialPos: CGPoint(x: self.frame.midX - self.frame.width / 2, y: self.frame.midY - self.frame.height / 2))
+        contact.bodyB.node?.removeFromParent()
+        //spawnObjectWithVelocity(originalObject: rock, initialPos: CGPoint(x: self.frame.midX - self.frame.width / 2, y: self.frame.midY - self.frame.height / 2))
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -104,11 +167,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     }
     
+    func gameStarts() {
+        var wait = SKAction.wait(forDuration: self.rockSpawnInterval)
+        let run = SKAction.run {
+            self.timer += CGFloat(self.rockSpawnInterval)
+            if self.rockSpawnInterval > 1 {
+                self.rockSpawnInterval -= 0.05
+            }
+            wait = SKAction.wait(forDuration: self.rockSpawnInterval)
+            self.spawnRockRandomly()
+        }
+        self.run(SKAction.repeatForever(SKAction.sequence([wait, run])))
+        
+    }
+    func gameEnds() {
+        self.removeAllActions()
+        
+    }
+    func gameResets() {
+        
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchInProgress = true
-        //earthShield.run()
-        //earthShield.zRotation -= 0.1
-
+        if gameInProgress {
+            spawnRockRandomly()
+        }
     }
     
     
@@ -123,8 +207,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     override func update(_ currentTime: TimeInterval) {
+        if touchInProgress && !gameInProgress {
+            gameInProgress = true
+            self.addChild(earthShield)
+            self.addChild(timerNode)
+            startButton.removeFromParent()
+            //gameStarts()
+        }
         // Called before each frame is rendered
-        if touchInProgress {
+        if touchInProgress && gameInProgress {
             toggleRotationClockwise(object: earthShield)
         }
     }
