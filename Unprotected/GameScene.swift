63852e39296d4f234740da05d0686d41b38d65ed
Chengming Lu <cluster626@gmail.com>
2017-02-21 21:52:01 -0500

    Now timer and score are working Need to work on restarting game and saving high score

--- Unprotected/GameScene.swift
+++ Unprotected/GameScene.swift
@@ -12,37 +12,28 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     var touchInProgress = false
     var gameInProgress = false
+    var readyForRestart = false
+    var beforeStart = true
     var earth = SKSpriteNode()
     var earthShield = SKSpriteNode()
     var startButton = SKSpriteNode()
     var timerLabel = SKLabelNode()
     var scoreLabel = SKLabelNode()
     var separatorLabel = SKLabelNode()
-    
-    var contactDetectionActive = false
-    
-    //test
-    var allContactCount = Int(0)
-    var shieldContactCount = Int(0)
-    var numberOfTouchesOrRockSpawns = Int(0)
-    var rockCount = Int(0)
-    
-    
-    //var rock = SKSpriteNode()
+    var centerPoint = CGPoint()
+
     var rockTexture = SKTexture()
     var timerNode = SKNode()
     var rockVelocity = CGFloat()
-    //var rockSpawnInterval = TimeInterval(5.0)
     var halfFrameDiagnal = CGFloat()
     var dynamicDifficulty = CGFloat()
-    var timer = CGFloat(60)
+    var timer = Int(10)
     var score = Int(0)
     var diagnalAngle = CGFloat()
     let objectRelativeScale = CGFloat(0.0002)
     let rockVelocityScale = CGFloat(0.3)
     let initialShieldOffset = CGFloat(-1.82)
     let rotationAngle = CGFloat(M_PI / 32)
-    //let rotationDuration = TimeInterval(3)
     let timerLabelYPosOffset = CGFloat(6)
     let scoreLabelYPosOffset = CGFloat(-37)
     let separatorLabelYPosOffset = CGFloat(-3)
@@ -60,7 +51,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.physicsWorld.contactDelegate = self
         halfFrameDiagnal = sqrt(pow(self.frame.size.width / 2, 2) + pow(self.frame.size.height / 2, 2))
         diagnalAngle = tanh(self.frame.size.width / self.frame.size.height)
-        
+        centerPoint = CGPoint(x: self.frame.midX, y: self.frame.midY)
         //timerNode.zPosition = -1
         //testing
         //let testEarth = SKShapeNode(circleOfRadius: 10)
@@ -69,42 +60,47 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //testEarth.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         //finished testing
         
+        //texture settings
         let earthTexture = SKTexture(imageNamed: "EarthFG.png")
-        let earthShieldTexture = SKTexture(imageNamed: "EarthShieldSingle.png")
+        let earthShieldTexture = SKTexture(imageNamed: "EarthShield.png")
         let startButtonTexture = SKTexture(imageNamed: "StartButton.png")
         rockTexture = SKTexture(imageNamed: "Rock.png")
         
+        //initialize sprite nodes
         earth = SKSpriteNode(texture: earthTexture)
         earthShield = SKSpriteNode(texture: earthShieldTexture)
         startButton = SKSpriteNode(texture: startButtonTexture)
         
+        //label settings
         timerLabel = SKLabelNode(fontNamed: "AppleSDGothicNeo-Regular")
         scoreLabel = SKLabelNode(fontNamed: "AppleSDGothicNeo-Regular")
         separatorLabel = SKLabelNode(fontNamed: "AppleSDGothicNeo-Bold")
-        timerLabel.text = "60"
+        timerLabel.text = "\(timer)"
         scoreLabel.text = "0"
         separatorLabel.text = "___"
         
-        earth.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        //position, zposition and scale settings
+        earth.position = centerPoint
         earth.zPosition = 4
-        earthShield.position = earth.position
+        earthShield.position = centerPoint
         earthShield.zPosition = 3
-        startButton.position = earth.position
-        startButton.zPosition = 5
+        startButton.position = centerPoint
+        startButton.zPosition = 7
         timerNode.zPosition = -1
-        timerLabel.position = earth.position
+        timerLabel.position = centerPoint
         timerLabel.position.y += timerLabelYPosOffset
         timerLabel.zPosition = 6
-        scoreLabel.position = earth.position
+        scoreLabel.position = centerPoint
         scoreLabel.position.y += scoreLabelYPosOffset
         scoreLabel.zPosition = 6
-        separatorLabel.position = earth.position
+        separatorLabel.position = centerPoint
         separatorLabel.position.y += separatorLabelYPosOffset
         separatorLabel.zPosition = 6
         earth.setScale(self.frame.size.width * objectRelativeScale)
-        earthShield.setScale(earth.xScale)
         startButton.setScale(earth.xScale)
+        earthShield.setScale(earth.xScale)
         
+        //rotation and physics settings
         earthShield.zRotation = initialShieldOffset
         earth.physicsBody = SKPhysicsBody(circleOfRadius: earth.size.width / 2, center: earth.position)
         earth.physicsBody!.isDynamic = false
@@ -117,11 +113,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         earthShield.physicsBody!.contactTestBitMask = CollisionType.Rock.rawValue
         earthShield.physicsBody!.collisionBitMask = CollisionType.Rock.rawValue
         
-        //we can actually add shield here , just hide it behind the earth, better for animation
+        //initial alpha settings
+        timerLabel.alpha = 0
+        scoreLabel.alpha = 0
+        separatorLabel.alpha = 0
         
+        //we can actually add shield here , just hide it behind the earth, better for animation
+        earthShield.setScale(0)
+        self.addChild(earthShield)
         self.addChild(earth)
         self.addChild(startButton)
-        
+        self.addChild(timerNode)
+        self.addChild(timerLabel)
+        self.addChild(scoreLabel)
+        self.addChild(separatorLabel)
         //self.frame:width = 750, height = 1334
         
         //print(timerLabel.position)
@@ -158,9 +163,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let randomDegree = CGFloat(2 * M_PI * drand48())
         let randomizedSpawnPoint = degreeToRandomizedPointOnRectangle(degreeInRadians: randomDegree, radius: halfFrameDiagnal)
         spawnRockWithVelocity(Pos: randomizedSpawnPoint)
-        rockCount += 1
-        print("rocks spawned in total: \(rockCount)")
-    //
     }
     
     func degreeToRandomizedPointOnRectangle(degreeInRadians: CGFloat, radius: CGFloat) -> CGPoint {
@@ -189,30 +191,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
-        print(contact.bodyA)
-        print(contact.bodyA.node?.name ?? "noname for A")
-        print("_______________________________")
-        print(contact.bodyB)
-        print(contact.bodyB.node?.name ?? "noname for B")
         
-        if contactDetectionActive {
-            contactDetectionActive = false
-            if contact.bodyA.categoryBitMask == CollisionType.Shield.rawValue {
-                //print("blocked by shield")
-                
-                score += 1
-                scoreLabel.text = "\(score)"
-                contactDetectionActive = true
-                shieldContactCount += 1
-                print("shield contact: \(shieldContactCount)")
-            } else if contact.bodyA.categoryBitMask == CollisionType.Earth.rawValue {
-                //contactDetectionActive = false
-                //print("absorbed by earth")
-            }
+        if contact.bodyA.categoryBitMask == CollisionType.Shield.rawValue && contact.bodyB.node?.parent != nil {
+            score += 1
+            scoreLabel.text = "\(score)"
+        } else if contact.bodyA.categoryBitMask == CollisionType.Earth.rawValue && contact.bodyB.node?.parent != nil && gameInProgress {
+            score -= 1
+            scoreLabel.text = "\(score)"
         }
-
-        allContactCount += 1
-        print("global contact count: \(allContactCount)")
         contact.bodyB.node?.removeFromParent()
         //spawnObjectWithVelocity(originalObject: rock, initialPos: CGPoint(x: self.frame.midX - self.frame.width / 2, y: self.frame.midY - self.frame.height / 2))
     }
@@ -226,15 +212,40 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func touchUp(atPoint pos : CGPoint) {
     }
     
-    func gameStarts() {
+    func timerStarts() {
         let wait = SKAction.wait(forDuration: unitTimeInterval)
         let run = SKAction.run {
-            //self.timer += CGFloat(self.rockSpawnInterval)
-
-            //wait = SKAction.wait(forDuration: self.rockSpawnInterval)
-            //self.spawnRockRandomly()
+            if self.timer <= 1 {
+                self.timerLabel.text = "0"
+                self.gameInProgress = false
+                self.removeAction(forKey: "TimerAction")
+                //self.removeAllActions()
+                let shieldRetract = SKAction.scale(to: 0, duration: 0.6)
+                let timerLabelFade = SKAction.fadeAlpha(to: 0, duration: 0.3)
+                let separatorLabelFade = SKAction.fadeAlpha(to: 0, duration: 0.3)
+                let scoreUpLift = SKAction.moveBy(x: self.score >= 0 ? 0 : -10, y: 16, duration: 0.3)
+                let scoreEmphasize = SKAction.scale(to: 1.5, duration: 0.5)
+                let scoreActionSet = SKAction.sequence([scoreUpLift, scoreEmphasize])
+                self.earthShield.run(shieldRetract, completion: {
+                    self.earthShield.removeFromParent()
+                })
+                self.timerLabel.run(timerLabelFade, completion: {
+                    self.timerLabel.removeFromParent()
+                })
+                self.separatorLabel.run(separatorLabelFade, completion: {
+                    self.separatorLabel.removeFromParent()
+                })
+                self.scoreLabel.run(scoreActionSet, completion: {
+                    self.readyForRestart = true
+                })
+                print(self.scoreLabel.xScale)
+                
+            }
+            self.timer -= 1
+            self.timerLabel.text = "\(self.timer)"
         }
-        self.run(SKAction.repeatForever(SKAction.sequence([wait, run])))
+        let repeatedAction = SKAction.repeatForever(SKAction.sequence([wait, run]))
+        self.run(repeatedAction, withKey: "TimerAction")
         
     }
     func gameEnds() {
@@ -247,11 +258,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchInProgress = true
-        numberOfTouchesOrRockSpawns += 1
-        print("touches: \(numberOfTouchesOrRockSpawns)")
         if gameInProgress {
             spawnRockRandomly()
-            //contactDetectionActive = true
         }
     }
     
@@ -267,16 +275,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     override func update(_ currentTime: TimeInterval) {
-        if touchInProgress && !gameInProgress {
+        if touchInProgress && beforeStart {
+            beforeStart = false
             gameInProgress = true
-            self.addChild(earthShield)
-            self.addChild(timerNode)
-            self.addChild(scoreLabel)
-            self.addChild(separatorLabel)
-            self.addChild(timerLabel)
-            startButton.removeFromParent()
-            contactDetectionActive = true
-            //gameStarts()
+            let startButtonScale = SKAction.scale(to: 0, duration: 0.3)
+            let shieldExtractAction = SKAction.scale(to: self.earth.xScale, duration: 0.6)
+            let timerAppear = SKAction.fadeAlpha(to: 1, duration: 0.3)
+            let separatorAppear = SKAction.fadeAlpha(to: 1, duration: 0.3)
+            let scoreAppear = SKAction.fadeAlpha(to: 1, duration: 0.3)
+            startButton.run(startButtonScale, completion: {
+                self.startButton.removeFromParent()
+                //self.addChild(self.earthShield)
+                self.earthShield.run(shieldExtractAction)
+                self.timerLabel.run(timerAppear)
+                self.separatorLabel.run(separatorAppear)
+                self.scoreLabel.run(scoreAppear)
+                self.timerStarts()
+            })
+        }
+        if !gameInProgress {
+            
         }
         // Called before each frame is rendered
         if touchInProgress && gameInProgress {
